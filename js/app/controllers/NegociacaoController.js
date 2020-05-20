adc2f924cd670d3e462631020fb831d75f073bef
Roberto Maycon <robertomaycon@hotmail.com>
2020-05-20 20:35:48 +1200

    criacao do metodo apaga para apagar a lista de negociacoes adicionadas.

--- js/app/controllers/NegociacaoController.js
+++ js/app/controllers/NegociacaoController.js
@@ -26,6 +26,14 @@ class NegociacaoController {
         
         this._limpaFormulario();   
     }
+
+    apaga() {
+        this._listaNegociacoes.esvazia();
+        this._negociacoesView.update(this._listaNegociacoes);
+
+        this._mensagem.texto = "Negociacoes apagadas com sucesso."
+        this._mensagemView.update(this._mensagem);
+    }
     
     _criaNegociacao() {
         
